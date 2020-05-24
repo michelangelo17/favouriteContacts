53a203c522c4cefdac2941d4a57b8eaac64382ec
michelangelo17 <michelangelo.markus@gmail.com>
2020-05-23 21:07:39 -0700

    full crud operational

--- routes/api/friends/middleware.js
+++ routes/api/friends/middleware.js
@@ -1,16 +1,21 @@
 const { findUserFriend } = require('./dbQueries')
 
 const checkUserAccess = async (req, res, next) => {
-  const searchResult = await findUserFriend()
+  const searchResult = await findUserFriend(
+    req.params.id,
+    req.decodedToken.subject
+  )
+  console.log(searchResult)
+  console.log(req.params.id, req.decodedToken.subject)
   if (!searchResult) throw new Error('No friends with that ID!')
   next()
 }
 
 const valBody = async (req, res, next) => {
-  if (!req.body.id || !req.body.name || !req.body.age || !req.body.email) {
-    throw new Error('required fields: id, name, age, email')
+  if (!req.body.name || !req.body.age || !req.body.email) {
+    throw new Error('required fields: name, age, email')
   }
   next()
 }
 
-module.exports = { authenticate, checkUserAccess, valBody }
+module.exports = { checkUserAccess, valBody }
